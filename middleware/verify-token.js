b5719a9e3ebbca76bf5a99b0d26e064b51cb6475
meshu-dev <admin@requiredev.com>
2020-04-11 10:14:44 +0100

    Fixed issue with expired web token

--- middleware/verify-token.js
+++ middleware/verify-token.js
@@ -1,19 +1,26 @@
-export default async function (context) {
-  let token = localStorage.getItem('auth._token.local');
+export default function (context) {
+  return new Promise(async (resolve, reject) => {
+    let token = localStorage.getItem('auth._token.local');
 
-  if (token && token !== 'false') {
-  	token = token.replace('Bearer ', '');
+    if (token && token !== 'false') {
+    	token = token.replace('Bearer ', '');
 
-  	let payload = token.split('.')[1],
-  		tokenData = JSON.parse(atob(payload));
+    	let payload = token.split('.')[1],
+    		tokenData = JSON.parse(atob(payload));
 
-	if (tokenData.exp) {
-		let expiryTime = tokenData.exp * 1000,
-			currentTime = Date.now();
+    	if (tokenData.exp) { // Date.now() + 100000
+    		let expiryTime = tokenData.exp * 1000, //1586596035534, //tokenData.exp * 1000,
+    			currentTime = Date.now();
 
-		if (expiryTime < currentTime) {
-			await context.$auth.logout();
-		}
-	}
-  }
+    		console.log('expiryTime', new Date(expiryTime));
+    		console.log('currentTime', new Date(currentTime));
+
+    		if (expiryTime < currentTime) {
+    			await context.$auth.logout();
+          context.redirect('/login');
+    		}
+    	}
+    }
+    resolve();
+  });
 }
