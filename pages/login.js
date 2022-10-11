0ecf59af8d24c469021b5f7939430cfc2ccdadf6
meshu-dev <harmeshuppal@gmail.com>
2022-10-11 20:48:16 +0100

    Added login form

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,47 @@
+import React from "react"
 import Head from 'next/head'
+import { Box } from '@mui/system';
+import Container from '@mui/material/Container';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
-import Layout from '../components/Layout/layout';
+export default class LoginPage extends React.Component {
+  handleSubmit = event => {
+    event.preventDefault()
 
-import SiteBlock from '../components/Site/SiteBlock';
-import EnvSelector from '../components/EnvSelector/EnvSelector';
+    this.setState({
+      isLoading: true,
+    })
+    this.recaptcha.execute()
+  }
 
-export default () => (
-  <Layout>
-    <EnvSelector />
-    <div>Hello World</div>
-    <SiteBlock />
-  </Layout>
-)
+  render() {
+    return (
+      <main id="login">
+        <Head>
+          <title>DevAdmin - Login</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Container maxWidth="sm">
+          <TextField
+              id="email-field"
+              className="login-field"
+              label="Email"
+              type="email"
+              fullWidth />
+          <TextField
+            id="password-field"
+            className="login-field"
+            label="Password"
+            type="password"
+            fullWidth />
+          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+            <Button
+              variant="contained"
+              onClick={this.handleSubmit}>Submit</Button>
+          </Box>
+        </Container>
+      </main>
+    )
+  }
+}
