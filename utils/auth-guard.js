b3c02e5b67c91cce4b17349197b214731a89d9df
meshu-dev <harmeshuppal@gmail.com>
2023-03-20 02:53:10 +0000

    Localstorage checj

--- utils/auth-guard.js
+++ utils/auth-guard.js
@@ -2,19 +2,21 @@ import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 
 const checkUrl = (router) => {
-  const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true' || false;
-  const path = router.pathname;
-
-  let redirectUrl = '';
+  if (localStorage) {
+    const isLoggedIn = localStorage.getItem('isLoggedIn') === 'true' || false;
+    const path = router.pathname;
   
-  if (path === '/' && isLoggedIn === false) {
-    redirectUrl = '/login';
-  } else if (path === '/login' && isLoggedIn === true) {
-    redirectUrl = '/';
-  }
-
-  if (redirectUrl) {
-    router.push(redirectUrl);
+    let redirectUrl = '';
+    
+    if (path === '/' && isLoggedIn === false) {
+      redirectUrl = '/login';
+    } else if (path === '/login' && isLoggedIn === true) {
+      redirectUrl = '/';
+    }
+  
+    if (redirectUrl) {
+      router.push(redirectUrl);
+    }
   }
 };
 
