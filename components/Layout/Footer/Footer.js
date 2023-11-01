99e4dee9549d9f3948b4fb93bda186c6265a6ff8
Mesh <harmeshuppal@gmail.com>
2023-11-01 20:05:28 +0000

    Remove old files

--- components/Layout/Footer/Footer.js
+++
@@ -1,5 +0,0 @@
-import styles from "./Footer.module.scss"
-
-export default () => (
-  <footer id={styles.footer}></footer>
-)
--- components/Layout/Header/Header.js
+++
@@ -1,7 +0,0 @@
-import styles from "./Header.module.scss"
-
-export default () => (
-  <h1 className={ styles.title }>
-    Dev Admin
-  </h1>
-)
